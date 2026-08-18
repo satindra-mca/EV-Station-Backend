a600df4eaed05ae42e33f0045634ba0cfe0c7f86
satindra <valvisatindra@gmail.com>
2026-08-18 18:59:22 +0530

    Initialize Spring Boot project and configure SQL connection

---
+++ src/main/java/com/example/Ev_Station_Backend/EvStationBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.Ev_Station_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvStationBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvStationBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Ev_Station_Backend/EvStationBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Ev_Station_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvStationBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
