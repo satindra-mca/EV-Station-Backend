ee46593b415699d45eb4c8849c92b05d6f35cd95
satindra <valvisatindra@gmail.com>
2026-09-02 22:06:00 +0530

    feat(exception): add global exception handling

---
+++ src/main/java/com/example/Ev_Station_Backend/exception/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package com.example.Ev_Station_Backend.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+    
+}
